f10482bfdc1fe2abf47428b8683ded51a1a9292b
Progga-Paromita <proggaparomitaarish@gmail.com>
2024-11-17 21:35:25 +0600

    Add files via upload

---
+++ Progga.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Progga
+{
+   public static void main(String[] args) {
+    System.out.print("For switch case Press a buttun:");
+    Scanner s=new Scanner(System.in);
+     int n=s.nextInt();
+     for(int i=0;i<n;i++)
+     {
+        for(int j=0;j<n;j++)
+        {
+          if(i==0||i==n-1||j==0||j==n-1)
+              System.out.print("* ");
+         else
+          System.out.print("  ");
+            
+        }
+        System.out.println();
+     }   
+   }
+}
